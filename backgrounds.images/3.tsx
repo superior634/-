2cda77494ccc90153c74eb123570652dbd88a374
superior634 <ivanbelanin114@gmail.com>
2026-02-19 17:39:40 +0300

    Add files via upload

---
+++ backgrounds.images/3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="3" tilewidth="20" tileheight="20" tilecount="2601" columns="51">
+ <image source="spr_tileset_sunnysideworld.png" width="1024" height="1024"/>
+</tileset>
---
+++ backgrounds.images/island 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="island 2" tilewidth="16" tileheight="16" tilecount="156" columns="13">
+ <image source="Island_24x24.png" width="216" height="192"/>
+</tileset>
