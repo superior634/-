da67db8863546adf78e66b9ef8a7103a3d57b1c1
DenArchi <xcvb1470hg@gmail.com>
2026-02-25 16:38:06 +0300

    Add files via upload
    вот это те

---
+++ backgrounds.images/1 new.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="1 new" tilewidth="16" tileheight="16" spacing="1" tilecount="900" columns="30">
+ <image source="D:/Downloads/tileset.png" width="512" height="512"/>
+</tileset>
---
+++ backgrounds.images/spr_tileset_sunnysideworld.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="spr_tileset_sunnysideworld" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
+ <image source="spr_tileset_sunnysideworld.png" width="1024" height="1024"/>
+</tileset>
